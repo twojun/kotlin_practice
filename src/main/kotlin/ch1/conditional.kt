e1f51b0f96d52aab31c9967d2822edc28e0a667f
yoowonjun <twojunspace@naver.com>
2024-03-16 12:55:35 +0900

    Conditional Statement

--- src/main/kotlin/ch1/conditional.kt
+++ src/main/kotlin/ch1/conditional.kt
@@ -1,5 +1,25 @@
 package org.example.ch1
 
 fun main() {
+    var number: Int = 5
 
+    if (number > 10) {
+        print("10보다 크다.")
+    } else if (number > 5) {
+        print("5보다 크다.")
+    } else {
+        print("아무것도 해당되지 않는다.")
+    }
+
+    when {
+        number > 10 -> {
+            print("10보다 크다.")
+        }
+        number > 5 -> {
+            print("5보다 크다.")
+        }
+        else -> {
+            print("아무것도 해당되지 않는다.")
+        }
+    }
 }
