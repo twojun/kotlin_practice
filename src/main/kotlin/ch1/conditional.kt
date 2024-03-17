266e1125ddbc36809c24d3df59e066211b2ef43c
yoowonjun <twojunspace@naver.com>
2024-03-17 23:06:39 +0900

    Ternary operator

--- src/main/kotlin/ch1/conditional.kt
+++ src/main/kotlin/ch1/conditional.kt
@@ -2,6 +2,7 @@ package org.example.ch1
 
 fun main() {
     var number: Int = 5
+    var boolCond = if (number > 5) true else false
 
     if (number > 10) {
         print("10보다 크다.")
@@ -22,4 +23,6 @@ fun main() {
             print("아무것도 해당되지 않는다.")
         }
     }
+
+    print(boolCond)
 }
